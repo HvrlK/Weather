f8362d828a73b8ef6785c701b482fe84caaf2c8c
HvrlK <g.v.s.23.07@gmail.com>
2018-07-11 15:03:24 +0300

    Configured cell and created segues.

--- Weather/CityTableViewController.swift
+++ Weather/CityTableViewController.swift
@@ -11,14 +11,19 @@ import CoreLocation
 
 class CityTableViewController: UITableViewController {
     
-    let locationManager = CLLocationManager()
+    //MARK: - Properties
     
+    let locationManager = CLLocationManager()
     var weatherInCurrentLocation: Weather?
-
     let weatherController = OpenWeatherMapController()
+    var weatherList: [Weather]?
+    
+    //MARK: - Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        weatherController.search(cityIds: "524901,703448,2172797")
+        weatherController.delegate = self
     }
 
     // MARK: - Table view data source
@@ -28,6 +33,9 @@ class CityTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let weatherList = weatherList {
+            return weatherList.count
+        }
         return 0
     }
 
@@ -35,26 +43,47 @@ class CityTableViewController: UITableViewController {
         locationManager.startUpdatingLocation()
     }
     
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        if let weatherList = weatherList {
+            cell.textLabel?.text = weatherList[indexPath.row].name + ", " + weatherList[indexPath.row].sys.country
+            cell.detailTextLabel?.text = Int(weatherList[indexPath.row].main.temp).description + "°C"
+        }
         return cell
     }
-    */
-
     
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let detailWeatherViewController = segue.destination as? DetailWeatherViewController {
-            detailWeatherViewController.weatherForCurrentLocation()
+        if segue.identifier == "ShowDetail" {
+            if let detailWeather = segue.destination as? DetailWeatherViewController, let indexPath = tableView.indexPathForSelectedRow, let weatherList = weatherList {
+                let weather = weatherList[indexPath.row]
+                detailWeather.weatherController.search(longitude: weather.coord.lon, latitude: weather.coord.lat)
+            }
+        }
+        if segue.identifier == "CurrentWeather" {
+            if let detailWeatherViewController = segue.destination as? DetailWeatherViewController {
+                detailWeatherViewController.weatherForCurrentLocation()
+            }
         }
     }
- 
 
+}
+
+//MARK: - Extensions
+
+extension CityTableViewController: OpenWeatherMapDelegate {
+    
+    func fetched(_ controller: OpenWeatherMapController) {
+        guard let weatherList = controller.weatherList else { return }
+        self.weatherList = weatherList
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    
 }
 
 
